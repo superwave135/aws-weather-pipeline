c96cb499bd6a957b96c1feb2e43f6c895c124a61
superwave135 <superwave135@yahoo.com.sg>
2025-12-13 11:13:35 +0800

    update locals.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,8 +1,6 @@
 locals {
   pipeline_name = var.pipeline_name
-
   lambda_script_path_in_bucket = "${var.pipeline_name}/lambda/lambda_code.zip"
-  lambda_script_path_in_github = "AWS/${var.pipeline_name}/lambda"
-  glue_transform_weather_data_script_path_in_github   = "AWS/${var.pipeline_name}/glue/transform-weather-data"
+  lambda_script_path_in_github = "lambda"
+  glue_transform_weather_data_script_path_in_github = "glue/transform-weather-data"
 }
-
